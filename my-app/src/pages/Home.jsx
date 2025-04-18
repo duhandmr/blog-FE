a36c3984d3a1fb7307ea823ed733291cd15a99b6
duhandmr <duhand012@gmail.com>
2025-04-18 23:39:26 +0300

    first-commit

---
+++ my-app/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <form>
+      <h1>Login</h1>
+      <div>
+        <input type="email" required/>
+      </div>
+    </form>
+  )
+}
+
+export default Home
